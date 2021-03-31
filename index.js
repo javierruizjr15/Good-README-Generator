a2373474c2b7366f80edb1200b6ba55641967091
Javier Ruiz <javierruizjr15@gmail.com>
2021-03-31 11:35:00 -0700

    code

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
+const inquirer = require('inquirer')
+
+let projectinfo = []
+
 // array of questions for user
-const questions = [
-
-];
-
+const questions = () => {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'person',
+        message: 'Whats the title of your project?'
+      },
+      {
+        type: 'input',
+        name: 'person',
+        message: 'Whats the discription of your project?'
+      },
+      {
+        type: 'input',
+        name: 'person',
+        message: 'Whats the install instructions of your project?'
+      },
+      {
+        type: 'input',
+        name: 'person',
+        message: 'Whats the usage info of your project?'
+      },
+      {
+        type: 'input',
+        name: 'person',
+        message: 'Whats the contribution guidelines of your project?'
+      },
+      {
+        type: 'input',
+        name: 'person',
+        message: 'Whats the test instructions of your project?'
+      }
+    ])
+      // .then(res => {
+      //   console.log(res)
+      // } 
+      // })
+      // .catch(err => console.log(err))
+  } 
+questions()
 // function to write README file
 function writeToFile(fileName, data) {
 }
